34136cbe21ae722c4e6c1a4f1c650cec5d92c7a3
innov.barakmenachem <innov.barakmenachem@gmail.com>
2021-01-14 00:04:16 +0200

    try6

--- utils.py
+++ utils.py
@@ -28,13 +28,15 @@ def copytree(src, dst, symlinks=False, ignore=None):
     for item in os.listdir(src):
         s = os.path.join(src, item)
         d = os.path.join(dst, item)
+        print("creating new app in SRC: " + s)
+        print("creating new app in DST: " + d)
         if 'build' not in s:
             if os.path.isdir(s):
                 shutil.copytree(s, d, symlinks, ignore)
             else:
                 shutil.copy2(s, d)
 
-        print("creating new app in " + d)
+
 
 
 def inplace_change(filename, old_string, new_string):
